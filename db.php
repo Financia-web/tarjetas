2597c8087266586a5c0fcd23b91619820bec3f3d
cristhian <cristhaincarballo@constru.com.ar>
2025-05-29 11:43:36 -0300

    update. modificacion de la config del sitio.

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$host = "localhost";   
+/*$host = "localhost";   
 $usuario = "root"; 
 $clave = "";     
 $base = "tarjetas";
@@ -10,5 +10,39 @@ $conexion = new mysqli($host, $usuario, $clave, $base);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
-}
+}*/
+
+
+$titulo_site = "Simulador";
+$conf_destino_userfiles = "sitio";
+$conf_direccion = "Urquiza 827 - Concordia";
+$conf_telefono = "+ 54 0345 423 0003";
+$conf_cp = "3100";
+$conf_ciudad = "Concordia, Entre RÃ­os";
+$conf_responsable = "Cristhian";
+$conf_email = "cristhiancarballo@constru.com.ar";
+$conf_horario_atencion = '';
+
+
+     $conf_sitio = 'http://autos.financia.com.ar';
+     $server = 'localhost';
+
+     $bd = "275732_wordpress";
+     $bd_usuario = "275732-admin";
+     $bd_pass = "heCL16-}.,$)";
+     
+     
+     $conn = new mysqli($server,$bd_usuario,$bd_pass,$bd);
+     $conn->set_charset('utf8');
+
+    
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+$fechaactual = date("Y-m-d");
+$horaactual = date ("H:i:s");
+$mesactual = date ("m");
+$anioactual = date ("Y");
+$diaactual = date("j");
+
 ?>
+
